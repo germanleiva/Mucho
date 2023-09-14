d47cd01e100cc9cece78e93a5f6086e6f01689df
Unni <unni.r.krishnan@gmail.com>
2023-09-14 11:24:07 +0200

    Added test scene and script for automatic speech recognition using OpenAI Whisper (using https://github.com/Macoron/whisper.unity)

---
+++ Assets/Scripts/SpeechToText.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechToText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartRecording()
+    {
+        Debug.Log("Start Recording");
+    }
+
+    public void StopRecording()
+    {
+        Debug.Log("Stop Recording");
+    }
+
+    //public void 
+}
