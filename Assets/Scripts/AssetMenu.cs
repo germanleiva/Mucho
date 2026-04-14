bc225c09c26523efecd991c090ea361ca02caac6
JacopoMereu <jachi334@gmail.com>
2026-04-14 17:12:02 +0200

    When the UI toggle a new asset menu, the old one is disabled

--- Assets/Scripts/AssetMenu.cs
+++ Assets/Scripts/AssetMenu.cs
@@ -15,26 +15,36 @@ public class AssetMenu : MonoBehaviour
 
     public GameObject rootObject;
 
-    void Awake()
-    {
-        //set position 60cm in front of target
-        //transform.position = target.position + target.forward * 0.6f;
-    }
+    private static AssetMenu _currentlyOpenMenu;
 
     private void OnEnable()
     {
         #region Check if all references are set
-            if (asset == null) throw new Exception("AssetMenu: Asset reference is not set.");
-            if (target == null) throw new Exception("AssetMenu: Target is not set.");
-            if (lineRenderer == null) throw new Exception("AssetMenu: LineRenderer reference is not set.");
-            if (lineStart == null) throw new Exception("AssetMenu: LineStart is not set.");
+
+        if (asset == null) throw new Exception("AssetMenu: Asset reference is not set.");
+        if (target == null) throw new Exception("AssetMenu: Target is not set.");
+        if (lineRenderer == null) throw new Exception("AssetMenu: LineRenderer reference is not set.");
+        if (lineStart == null) throw new Exception("AssetMenu: LineStart is not set.");
+
         #endregion
 
         _lineEnd = asset.transform;
         UpdateTitle();
 
-        asset.OnMeshUpdated += _ => UpdateTitle();
+        asset.OnMeshUpdated += HandleMeshUpdated;
     }
+
+    private void OnDisable()
+    {
+        asset.OnMeshUpdated -= HandleMeshUpdated;
+        
+        if (_currentlyOpenMenu == this)
+        {
+            _currentlyOpenMenu = null;
+        }
+    }
+
+    private void HandleMeshUpdated(Asset _) => UpdateTitle();
 
     private void UpdateTitle()
     {
@@ -43,6 +53,7 @@ public class AssetMenu : MonoBehaviour
             menuTitleText.text = asset.gameObject.name;
         }
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -60,20 +71,56 @@ public class AssetMenu : MonoBehaviour
     public void MenuGrabbed(bool _isGrabbed)
     {
         isGrabbed = _isGrabbed;
+    } // TODO J - This isn't really used? The variable is class private, so it shouldn't be visible outside of this class..so what is this for?
+
+    private void ShowAssetMenu()
+    {
+        // If there is another menu open, and it's not this one (we just opened it), close it
+        if (_currentlyOpenMenu != null && _currentlyOpenMenu != this)
+        {
+            _currentlyOpenMenu.gameObject.SetActive(false);
+        }
+        // Set this menu as the currently open menu and show it
+        _currentlyOpenMenu = this;
+        
+        // Effectively "show" the menu by enabling the gameobject
+        gameObject.SetActive(true);
     }
 
-    public void toggleAssetMenu()
+    private void HideAssetMenu()
     {
-        gameObject.SetActive(!gameObject.activeSelf);
-    }
+        // If this is the currently open menu, clear the reference to it since we're closing it
+        if (_currentlyOpenMenu == this)
+        {
+            _currentlyOpenMenu = null;
+        }
 
-    public void hideAssetMenu()
-    {
+        // Effectively "hide" the menu by disabling the gameobject
         gameObject.SetActive(false);
+    }
+
+    public void ToggleAssetMenu()
+    {
+        if (gameObject.activeSelf)
+        {
+            HideAssetMenu();
+        }
+        else
+        {
+            ShowAssetMenu();
+        }
     }
 
     public void DeleteRootObject()
     {
         Destroy(rootObject);
     }
+    
+    #region Context Menu Button Callbacks
+    [ContextMenu("Toggle Asset Menu")]
+    private void ContextMenuToggleAssetMenu()
+    {
+        ToggleAssetMenu();
+    }
+    #endregion
 }
